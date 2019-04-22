bff534bfe33b1b9b72777e8ae9cd71a71dc08cbe
Peter Pae <paendabear@gmail.com>
2019-04-22 16:54:14 -0400

    finish conditional rendering with no errors

--- googlebooks-search/src/Book/Book.js
+++ googlebooks-search/src/Book/Book.js
@@ -6,10 +6,16 @@ class Book extends Component {
     render() {
 
     console.log(this.props.author)
-        let authors = this.props.author.join(', ');
+    let authors;
+    if(this.props.authors === undefined){
+         authors = 'no authors';
+    } else{
+         authors = this.props.author.join(', ');
+    }
+        
 
     console.log(this.props.price);
-    let price = this.props.price;
+   
     let saleability = this.props.saleability;
 
         
@@ -19,7 +25,6 @@ class Book extends Component {
                         <h2>{this.props.title}</h2>
                         
                         <img src={this.props.thumbnail_URL} alt='book should be here?' />
-        
                     <div>
                             <h3>Authors: {authors}</h3>
                             <h4>price: Free</h4>
@@ -30,7 +35,11 @@ class Book extends Component {
                 );
             }
             else {
-                return (     
+
+                let price = this.props.price;
+                
+                const priceTag = (price) ? <h4>price: { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(price.amount) }</h4> : false;
+                    return (     
             <li>
                 <h2>{this.props.title}</h2>
                 
@@ -38,15 +47,15 @@ class Book extends Component {
 
             <div>
                     <h3>Authors: {authors}</h3>
-                    <h4>price: { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                            .format(price.amount) }</h4>
+                    {priceTag}
                     {this.props.description}
                     
             </div>
             </li>
         );
+                }
     }
-    }
+    
 }
 
 
